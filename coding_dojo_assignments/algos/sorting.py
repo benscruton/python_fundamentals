1ee7d021f6815e3f068c89c2f4b5a7e614787cb4
benscruton <benjamin.scruton@gmail.com>
2021-09-28 23:51:20 -0500

    different inefficient sort

--- coding_dojo_assignments/algos/sorting.py
+++ coding_dojo_assignments/algos/sorting.py
@@ -1,3 +1,8 @@
+def sep(length = 30):
+    print()
+    print("*" * length)
+    print()
+
 def gen_list(length = 20, maximum = 100, minimum = 0):
     import random
     import math
@@ -17,8 +22,23 @@ def bubble_sort(l):
             if l[i] > l[i+1]:
                 completed = False
                 l[i], l[i+1] = l[i+1], l[i]
+
+def weird_bad_sort(l):
+    for i in range(len(l) - 1):
+        for j in range(i, len(l)):
+            if l[i] > l[j]:
+                l[i], l[j] = l[j], l[i]
     
-test = gen_list(10)
+test = gen_list(100)
 print(test)
+print()
 bubble_sort(test)
+print(test)
+
+sep()
+
+test = gen_list(100)
+print(test)
+print()
+weird_bad_sort(test)
 print(test)

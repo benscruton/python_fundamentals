97a1c778458797eaca6d77f1a1f3f80f4a03f6d2
benscruton <benjamin.scruton@gmail.com>
2021-10-03 20:05:20 -0500

    longest common prefix

---
+++ coding_dojo_assignments/algos/longest_common_prefix.py
@@ -0,0 +1,22 @@
+def longest_common_prefix(strings):
+    if not len(strings):
+        return ""
+    prefix = strings[0]
+    for i in range(1, len(strings)):
+        if not prefix:
+            break
+        string = strings[i]
+        for j in range(len(string)):
+            if j >= len(prefix) or prefix[j] != string[j]:
+                prefix = string[:j]
+                break
+    return prefix
+
+test1 = ["hello", "hello", "helloawef"]
+test2 = ["a", "b", "c"]
+test3 = ["flee", "flow", "flea"]
+
+tests = [test1, test2, test3]
+
+for test in tests:
+    print(longest_common_prefix(test))

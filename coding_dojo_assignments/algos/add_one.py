67dccf307cfddb0cdfbe7ef3ac56bfa4779fc516
benscruton <benjamin.scruton@gmail.com>
2021-10-04 21:03:29 -0500

    another add_one solution

--- coding_dojo_assignments/algos/add_one.py
+++ coding_dojo_assignments/algos/add_one.py
@@ -2,7 +2,7 @@
 
 # Increment the large integer by one and return the resulting array of digits.
 
-def add_one(digits):
+def add_one_orig(digits):
     place = len(digits) - 1
     digits[place] += 1
     while digits[place] == 10 and place > 0:
@@ -11,5 +11,15 @@ def add_one(digits):
         digits[place] += 1
     if digits[0] == 10:
         digits[0] = 0
+        digits.insert(0, 1)
+    return digits
+
+def add_one(digits):
+    i = len(digits) - 1
+    go = True
+    while go and i >= 0:
+        digits[i] = (digits[i] + 1) % 10
+        go = not digits[i]
+    if go:
         digits.insert(0, 1)
     return digits

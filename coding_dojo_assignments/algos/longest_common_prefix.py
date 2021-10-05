63ce6830b38ccfa50d392fd44ce4953f5ccfeff0
benscruton <benjamin.scruton@gmail.com>
2021-10-04 19:37:36 -0500

    prefix improvements

--- coding_dojo_assignments/algos/longest_common_prefix.py
+++ coding_dojo_assignments/algos/longest_common_prefix.py
@@ -6,6 +6,8 @@ def longest_common_prefix(strings):
         if not prefix:
             break
         string = strings[i]
+        if len(string) >= len(prefix) and string[:len(prefix)] == prefix:
+            continue
         for j in range(len(string)):
             if j >= len(prefix) or prefix[j] != string[j]:
                 prefix = string[:j]
